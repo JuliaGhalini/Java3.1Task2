cbc7848411f02ab2f37acb9e19f7a6f90cec4956
Ghalini Julia <ghalinijulia@gmail.com>
2020-11-28 14:11:57 +0300

    Initial File

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,10 +1,78 @@
 package ru.netology;
 
+
 public class Radio {
-        private String name;
-        private int maxTemperature;
-        private int minTemperature;
-        private int currentTemperature;
-        private boolean on;
+        private int currentRadio;
+        private int maxRadio;
+        private int minRadio;
+        private int currentVolume;
+        private int maxVolume;
+        private int minVolume;
+
+        public int getCurrentRadio() { return currentRadio; }
+
+        public void setCurrentRadio(int currentRadio) { this.currentRadio = currentRadio; }
+
+        public int getMaxRadio() { return maxRadio; }
+
+        public void setMaxRadio(int maxRadio) { this.maxRadio = maxRadio; }
+
+        public int getMinRadio() { return minRadio; }
+
+        public void setMinRadio(int minRadio) { this.minRadio = minRadio; }
+
+        public int getCurrentVolume() { return currentVolume; }
+
+        public void setCurrentVolume(int currentVolume) { this.currentVolume = currentVolume; }
+
+        public int getMaxVolume() { return maxVolume; }
+
+        public void setMaxVolume(int maxVolume) { this.maxVolume = maxVolume; }
+
+        public int getMinVolume() { return minVolume; }
+
+        public void setMinVolume(int minVolume) { this.minVolume = minVolume; }
+
+
+
+        public void nextCurrentRadio(){
+                if (currentRadio == 9){
+                        this.currentRadio = 0;
+                        return;
+                }
+                this.currentRadio = currentRadio + 1;
+        }
+
+        public void prevCurrentRadio(){
+                if(currentRadio == 0){
+                        this.currentRadio = 9;
+                        return;
+                }
+                this.currentRadio = currentRadio - 1;
+        }
+
+        public void increaseCurrentVolume(){
+                if(currentVolume == 10){
+                        return;
+                }
+                this.currentVolume = currentVolume + 1;
+        }
+
+        public  void decreaseCurrentVolume(){
+                if(currentVolume == 0){
+                        return;
+                }
+                this.currentVolume = currentVolume - 1;
+        }
+
+}
+
+
+
+
+
+
+
+
 
 
--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -1,8 +1,63 @@
 package ru.netology;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
+    private Radio radio = new Radio();
 
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "'expected, currentRadio', 7, 8",
+                    "'expected, currentRadio', 9, 0"
+            }
+    )
+    void nextCurrentRadio(String test, int currentRadio, int expected) {
+        radio.setCurrentRadio(currentRadio);
+        radio.nextCurrentRadio();
+        assertEquals(expected, radio.getCurrentRadio());
+    }
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "'expected, currentRadio', 7, 6",
+                    "'expected, currentRadio', 0, 9"
+            }
+    )
+    void shouldPrevCurrentRadio(String test, int currentRadio, int expected) {
+        radio.setCurrentRadio(currentRadio);
+        radio.prevCurrentRadio();
+        assertEquals(expected, radio.getCurrentRadio());
+    }
 
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "'expected, currentVolume', 10, 10",
+                    "'expected, currentRadio', 8, 9"
+            }
+    )
+
+    void increaseCurrentVolume(String test, int currentVolume, int expected) {
+        radio.setCurrentVolume(currentVolume);
+        radio.increaseCurrentVolume();
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "'expected, currentVolume', 0, 0",
+                    "'expected, currentRadio', 8, 7"
+            }
+    )
+    void decreaseCurrentVolume(String test, int currentVolume, int expected) {
+        radio.setCurrentVolume(currentVolume);
+        radio.decreaseCurrentVolume();
+        assertEquals(expected, radio.getCurrentVolume());
+    }
 }
